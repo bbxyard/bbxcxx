686108197174641b41a351d291f34d0f07022d4c
bbxyard <bbxyard@gmail.com>
2013-11-23 19:22:44 +0800

    [boost] 增加shared_ptr, assign学习.

--- boost/src/startup/main.cpp
+++ boost/src/startup/main.cpp
@@ -4,21 +4,34 @@
 
 #include <string>
 #include <iostream>
+#include <set>
+#include <map>
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+
+
+#if 0
 #include "test_bimap.hpp"
 #include "test_string.hpp"
 #include "test_autoptr.hpp"
+#endif
 
-
+#include "test_misc.hpp"
 
 int main(int argc, char* argv[])
 {
-    // 2013.09.02
-    bbxyard::bautoptr::test::main(argc, argv);
+    // 2013.11.23
+    bbxyard::misc::test::main(argc, argv);
 
-    // 2013.09.01
-    bbxyard::bstring::test::main(argc, argv);
+    //// 2013.09.02
+    //bbxyard::bautoptr::test::main(argc, argv);
 
-    // 2013.08
-    bbxyard::bbimap::test::main(argc, argv);
+    //// 2013.09.01
+    //bbxyard::bstring::test::main(argc, argv);
+
+    //// 2013.08
+    //bbxyard::bbimap::test::main(argc, argv);
     return 0;
 }
