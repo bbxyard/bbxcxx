53281a6c1f516b3075e3c2156bd7b0a5bb60d228
bbxyard <bbxyard@gmail.com>
2016-01-11 22:18:15 +0800

    update

--- boost/src/startup/main.cpp
+++ boost/src/startup/main.cpp
@@ -4,35 +4,21 @@
 
 #include <string>
 #include <iostream>
-#include <set>
-#include <map>
-#include <vector>
-#include <list>
-#include <queue>
-#include <stack>
-
 #include "test_bimap.hpp"
 #include "test_string.hpp"
 #include "test_autoptr.hpp"
 
-#include "test_misc.hpp"
-#include "test_python.hpp"
+
 
 int main(int argc, char* argv[])
 {
-    // 2013.12.1
-    bbxyard::bpython::test::main(argc, argv);
+    // 2013.09.02
+    bbxyard::bautoptr::test::main(argc, argv);
 
-    // 2013.11.23
-    bbxyard::misc::test::main(argc, argv);
+    // 2013.09.01
+    bbxyard::bstring::test::main(argc, argv);
 
-    //// 2013.09.02
-    //bbxyard::bautoptr::test::main(argc, argv);
-
-    //// 2013.09.01
-    //bbxyard::bstring::test::main(argc, argv);
-
-    //// 2013.08
-    //bbxyard::bbimap::test::main(argc, argv);
+    // 2013.08
+    bbxyard::bbimap::test::main(argc, argv);
     return 0;
 }
